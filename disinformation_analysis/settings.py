50797fcd71986975277d6ceca1b9ef021a1a5f6d
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-06-21 20:22:47 +0500

    Add researchdata app to project settings

--- disinformation_analysis/settings.py
+++ disinformation_analysis/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'researchdata.apps.ResearchdataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
