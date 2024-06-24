8a0bff545465ce7f07b362aedb0be7ae1093f6b8
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-06-24 15:10:09 +0500

    Add posts and responses to populate db script - Add posts info csv file

--- scripts/populate_healthcare_disinformation.py
+++ scripts/populate_healthcare_disinformation.py
@@ -11,17 +11,18 @@ django.setup()
 from researchdata.models import *
 
 project_path = os.getenv('PROJECT_PATH')
-heathcare_disinfo_data = project_path + "/Healthcare_Disinfo_Study_Data.csv"
+heathcare_disinfo_data = project_path + "/data/Healthcare_Disinfo_Study_Data.csv"
+posts_info_data = project_path + "/data/Posts_info.csv"
 
 users = defaultdict(list)
 personalities = defaultdict(list)
 responses = []
+posts = defaultdict(list)
 
 with open(heathcare_disinfo_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     next(csv_reader, None)
 
-    # get all unique user ids from csv file
     unique_user_ids = set()
     for row in csv_reader:
         if not unique_user_ids.__contains__(row[1]):
@@ -31,11 +32,26 @@ with open(heathcare_disinfo_data) as csv_file:
 
         responses.append(row[0:4] + row[9:10] + row[11:12])
 
-User.objects.all().delete()
+print("Successfully read disinformation data csv file")
+
+with open(posts_info_data) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    next(csv_reader, None)
+
+    for row in csv_reader:
+        posts[row[0]] = row[1:3]
+
+print("Successfully read posts data csv file")
+
 Personality.objects.all().delete()
 Response.objects.all().delete()
+Post.objects.all().delete()
+User.objects.all().delete()
+
+print("Successfully deleted all data from database")
 
 user_objects = {}
+posts_objects = {}
 
 for user_id, data in users.items():
     if not user_id.isnumeric():
@@ -59,6 +75,7 @@ for user_id, data in users.items():
     newUser.save()
     user_objects[user_id] = newUser
 
+print("Successfully saved users data")
 
 for user_id, data in personalities.items():
     if not user_id.isnumeric():
@@ -100,3 +117,56 @@ for user_id, data in personalities.items():
                                                 risk7=int(data[29]))
 
     newPersonality.save()
+
+print("Successfully saved personality data")
+
+
+for post_id, data in posts.items():
+    if not post_id.isnumeric():
+        continue
+
+    if data[1] != "TRUE" and data[1] != "FALSE":
+        continue
+
+    natureDict = {'TRUE': True, 'FALSE': False}
+
+    newPost = Post.objects.create(post_id=int(post_id),
+                                  name=data[0],
+                                  nature=natureDict.get(data[1]))
+
+    newPost.save()
+
+    posts_objects[post_id] = newPost
+
+
+print("Successfully saved posts data")
+
+all_responses = len(responses)
+responses_loaded = 0
+for response in responses:
+    if not response[0].isnumeric():
+        continue
+
+    if not response[1].isnumeric():
+        continue
+
+    if response[5] != '1' and response[5] != '2':
+        continue
+
+    post_status_opinionDict = {'0': 'dont know', '1': 'False', '2': 'True'}
+    correctnessDict = {'1': True, '2': False}
+    newResponse = Response.objects.create(post_id=posts_objects[response[0]],
+                                          user_id=user_objects[response[1]],
+                                          reason=response[2],
+                                          verbal_code=response[3],
+                                          post_status_opinion=post_status_opinionDict.get(response[4], 'dont know'),
+                                          correctness=correctnessDict.get(response[5]))
+
+    newResponse.save()
+    responses_loaded += 1
+
+    if (responses_loaded % 50 == 0):
+        print("Responses saved: ", responses_loaded, "/", all_responses)
+
+
+print("Successfully saved responses data")
