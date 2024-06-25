d8d814aea5fdee33cceb1ebe64c85db1427aed93
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-06-25 23:05:39 +0500

    Add response serializer

--- researchdata/serializers.py
+++ researchdata/serializers.py
@@ -11,3 +11,9 @@ class PersonalitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Personality
         fields = ["user_id", "mac1", "mac2", "mac3", "mac4"]
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = ["post_id", "user_id", "reason", "verbal_code", "post_status_opinion", "correctness"]
