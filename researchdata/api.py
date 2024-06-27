1781674334cab501094827b4ba86339f194821b1
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-06-27 17:22:14 +0500

    Refactor code - Replace csrf_exempt with rest framework api_view - Replace JsonResponse with rest framework Response

--- researchdata/api.py
+++ researchdata/api.py
@@ -1,104 +1,87 @@
-from django.http import JsonResponse, HttpResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response as Rest_Response
+from rest_framework import status
 from .serializers import *
 
 @csrf_exempt
 def check(request):
     return HttpResponse(status=200)
 
-@csrf_exempt
+@api_view(['GET'])
 def simple_user_info(request, user_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         user = User.objects.get(user_id=user_id)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_user = UserSerializer(user)
-    return JsonResponse(serialized_user.data)
+    return Rest_Response(serialized_user.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def simple_user_responses(request, user_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         user = User.objects.get(user_id=user_id)
         responses = Response.objects.filter(user_id=user_id)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_responses = ResponseSerializer(responses, many=True)
-    return JsonResponse(serialized_responses.data, safe=False)
+    return Rest_Response(serialized_responses.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def simple_post_info(request, post_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         post = Post.objects.get(post_id=post_id)
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_post = PostSerializer(post)
-    return JsonResponse(serialized_post.data)
+    return Rest_Response(serialized_post.data)
 
 
-@csrf_exempt
+@api_view(['GET'])
 def user_info(request, user_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         user = User.objects.get(user_id=user_id)
         personality = Personality.objects.get(user_id=user_id)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
     except Personality.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_user = UserSerializer(user)
     serialized_personality = PersonalitySerializer(personality)
     combined_data = {"user": serialized_user.data,
                      "personality": serialized_personality.data}
 
-    return JsonResponse(combined_data)
+    return Rest_Response(combined_data)
 
-@csrf_exempt
+@api_view(['GET'])
 def user_responses(request, user_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         user = User.objects.get(user_id=user_id)
         responses = Response.objects.filter(user_id=user_id)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_user = UserSerializer(user)
     serialized_responses = ResponseSerializer(responses, many=True)
     combined_data = {"user": serialized_user.data,
                      "responses": serialized_responses.data}
 
-    return JsonResponse(combined_data)
+    return Rest_Response(combined_data)
 
-@csrf_exempt
+@api_view(['GET'])
 def post_info(request, post_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         post = Post.objects.get(post_id=post_id)
         true_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="True")
         false_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="False")
         questionable_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="dont know")
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_post = PostSerializer(post)
 
@@ -114,20 +97,17 @@ def post_info(request, post_id):
                      "false_assumption_user_ids": list(false_assumption_user_ids),
                      "questionable_assumption_user_ids": list(questionable_assumption_user_ids)}
 
-    return JsonResponse(combined_data)
+    return Rest_Response(combined_data)
 
-@csrf_exempt
+@api_view(['GET'])
 def detailed_post_info(request, post_id):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     try:
         post = Post.objects.get(post_id=post_id)
         true_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="True")
         false_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="False")
         questionable_assumption_responses = Response.objects.filter(post_id=post_id, post_status_opinion="dont know")
     except Post.DoesNotExist:
-        return HttpResponse(status=404)
+        return Rest_Response(status=status.HTTP_404_NOT_FOUND)
 
     serialized_post = PostSerializer(post)
     serialized_true_assumption_responses = ResponseSerializer(true_assumption_responses, many=True)
@@ -149,13 +129,10 @@ def detailed_post_info(request, post_id):
                      "false_assumption_responses": serialized_false_assumption_responses.data,
                      "questionable_assumption_responses": serialized_questionable_assumption_responses.data}
 
-    return JsonResponse(combined_data)
+    return Rest_Response(combined_data)
 
-@csrf_exempt
+@api_view(['GET'])
 def users_ranked(request):
-    if request.method != "GET":
-        return HttpResponse(status=405)
-
     users = User.objects.all()
     json = {}
     count = 0
@@ -167,4 +144,4 @@ def users_ranked(request):
         json[count] = user_data
         count += 1
 
-    return JsonResponse(json)
+    return Rest_Response(json)
