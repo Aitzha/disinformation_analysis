d8a31321a0f5f4c2408289c7f6abd774fd8f5af7
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-07-01 15:25:11 +0500

    Polish tests

--- researchdata/tests.py
+++ researchdata/tests.py
@@ -79,7 +79,7 @@ class CheckAPITests(APITestCase):
 
 
 class FullUserAPITests(APITestCase):
-    def test_userAndPersonalityExist_returnSuccess(self):
+    def test_userAndPersonalityExist_returnCorrectJson(self):
         user = UserFactory.create()
         PersonalityFactory.create(user=user)
         url = reverse('full_user_api', kwargs={'user_id': user.user_id})
@@ -87,6 +87,7 @@ class FullUserAPITests(APITestCase):
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
         self.assertEqual(response_data['user']['user_id'], user.user_id)
+        self.assertEqual(response_data['personality']['user'], user.user_id)
 
     def test_noUserExist_returnNotFound(self):
         url = reverse('full_user_api', kwargs={'user_id': 0})
@@ -106,7 +107,7 @@ class FullUserAPITests(APITestCase):
 
 
 class FullPostAPITests(APITestCase):
-    def test_postExistAndNoResponse_returnCorrectPost(self):
+    def test_postExistAndNoResponse_returnCorrectJson(self):
         post = PostFactory.create()
         url = reverse('full_post_api', kwargs={'post_id': post.post_id})
         response = self.client.get(url)
@@ -145,23 +146,23 @@ class FullPostAPITests(APITestCase):
         self.assertEqual(response.status_code, 405)
 
 
-class FullUserResponseAPITests(APITestCase):
-    def test_userExistAndZeroResponses_returnSuccess(self):
+class UserResponseAPITests(APITestCase):
+    def test_userExistAndZeroResponses_returnCorrectJson(self):
         user = UserFactory.create()
-        url = reverse('full_user_response_api', kwargs={'user_id': user.user_id})
+        url = reverse('user_response_api', kwargs={'user_id': user.user_id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
         self.assertEqual(response_data['user']['user_id'], user.user_id)
         self.assertEqual(len(response_data['responses']), 0)
 
-    def test_userExistAndTwoResponses_returnSuccess(self):
+    def test_userExistAndTwoResponses_returnCorrectJson(self):
         user = UserFactory.create()
         post1 = PostFactory.create(post_id=0)
         post2 = PostFactory.create(post_id=1)
         ResponseFactory.create(user=user, post=post1)
         ResponseFactory.create(user=user, post=post2)
-        url = reverse('full_user_response_api', kwargs={'user_id': user.user_id})
+        url = reverse('user_response_api', kwargs={'user_id': user.user_id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         response_data = response.json()
@@ -169,12 +170,12 @@ class FullUserResponseAPITests(APITestCase):
         self.assertEqual(len(response_data['responses']), 2)
 
     def test_noUserExist_returnNotFound(self):
-        url = reverse('full_user_response_api', kwargs={'user_id': 0})
+        url = reverse('user_response_api', kwargs={'user_id': 0})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     def test_sendPostRequest_returnNotFound(self):
-        url = reverse('full_user_response_api', kwargs={'user_id': 0})
+        url = reverse('user_response_api', kwargs={'user_id': 0})
         response = self.client.post(url)
         self.assertEqual(response.status_code, 405)
 
